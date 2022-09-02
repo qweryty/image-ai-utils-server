c6909e3c992360ec5602383f672387060d35e710
Sergey Morozov <sergey@morozov.top>
2022-09-02 07:19:25 +0300

    Added windows build

---
+++ pyinstaller_hooks/hook-transformers.py
@@ -0,0 +1,14 @@
+from PyInstaller.utils.hooks import collect_all
+from transformers.dependency_versions_table import deps
+
+
+def hook(hook_api):
+    packages = deps.keys()
+    print('---------------------- IMPORTING TRANSFORMERS DEPS ----------------------')
+    print(list(packages))
+
+    for package in packages:
+        datas, binaries, hidden_imports = collect_all(package)
+        hook_api.add_datas(datas)
+        hook_api.add_binaries(binaries)
+        hook_api.add_imports(*hidden_imports)
